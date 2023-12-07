80993b68f43cb4bbbeb22cfb48f3809af380c217
PauloDaude <paulodaude14@gmail.com>
2023-12-07 17:22:59 -0300

    feat: hover color added in buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const Button = ({ style, text, icon = 'false' }) => {
   if (style === 'light') {
     return (
-      <button className="flex justify-center items-center bg-gray-bg w-full sm:max-w-[21.4375rem] p-4 mb-4 sm:mb-0 rounded-full font-bold shadow-gray gap-2">
+      <button className="flex justify-center items-center bg-gray-bg w-full sm:max-w-[21.4375rem] p-4 mb-4 sm:mb-0 rounded-full font-bold shadow-gray gap-2 hover:bg-gray-200 transition-all">
         {icon === 'google' && (
           <FontAwesomeIcon icon={faGoogle} style={{ color: '#000' }} />
         )}
@@ -16,7 +16,7 @@ const Button = ({ style, text, icon = 'false' }) => {
     );
   } else {
     return (
-      <button className="flex justify-center items-center bg-black text-white w-full sm:max-w-[21.4375rem] p-4 rounded-full font-bold shadow-yellow gap-2">
+      <button className="flex justify-center items-center bg-black text-white w-full sm:max-w-[21.4375rem] p-4 rounded-full font-bold shadow-yellow gap-2 hover:bg-[#1e2029] transition-all">
         {icon === 'true' && (
           <FontAwesomeIcon icon={faBolt} style={{ color: '#ffffff' }} />
         )}
